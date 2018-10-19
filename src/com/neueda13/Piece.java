2054fcab448bc883212c6a6e0d33f78fa48d87d1
Ryan McCauley <ryanmccauley211@gmail.com>
2018-10-19 22:25:25 +0100

    - Add Piece class

---
+++ src/com/neueda13/Piece.java
@@ -0,0 +1,13 @@
+package com.neueda13;
+
+public class Piece {
+
+    PieceType pieceType;
+    Position position;
+
+    // todo TASK
+    /**
+     * Create a constructor and relevant accessors
+     */
+
+}

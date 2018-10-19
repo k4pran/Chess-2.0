5c20b6d63e55d98c850a408d1a1e273843527953
Ryan McCauley <ryanmccauley211@gmail.com>
2018-10-19 22:26:32 +0100

    - Add Position class

---
+++ src/com/neueda13/Position.java
@@ -0,0 +1,11 @@
+package com.neueda13;
+
+public class Position {
+
+    // todo TASK
+    /**
+     * Represents a position on the chess board as (x,y) coordinates
+     * Top-left position should be (0, 0)
+     * Create properties, constructor and accessors that represents a single position
+     */
+}
